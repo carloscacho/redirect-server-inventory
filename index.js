7144b3db9b6780f4d8699eadc3fdc8ec7a86f2f5
carloscacho <carlosemilio.ac@gmail.com>
2024-10-30 10:42:53 -0400

    mostrando todas infos de erro

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/callback', (req, res) => {
     // Redireciona para a URL com os parâmetros adicionais
     res.redirect(redirectUrl);
   } else {
-    res.status(400).send(`Parâmetros ausentes. URL completa recebida: ${req.originalUrl}`);
+    res.status(400).send(`Parâmetros ausentes. URL completa recebida: ${JSON.stringify(req)}`);
   }
 });
 
